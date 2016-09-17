12b823737098cb5e9ee7734a31b25e7c9884b6bf
longnguyen0708 <thailong0708@gmail.com>
2016-09-17 00:05:16 -0700

    do geolocation

--- WebContent/js/jsfile.js
+++ WebContent/js/jsfile.js
@@ -2,52 +2,52 @@
  * 
  */
 //topic: Variables
-//var school;
-//document.write("<p>school before initialized: " + school + "</p>");
-//school = "SJSU";
-//document.write("<p>school after initialized: " + school + "</p>");
-//school = 10;
-//document.write("school after 2nd assignment: " + school);
+var school;
+document.write("<p>school before initialized: " + school + "</p>");
+school = "SJSU";
+document.write("<p>school after initialized: " + school + "</p>");
+school = 10;
+document.write("school after 2nd assignment: " + school);
 
 //topic: Object
-//function Employee(name, dob, salary_per_hour) {
-//	this.name = name;
-//	this.dob = dob;
-//	this.salary_per_hour = salary_per_hour;
-//	this.calMonthlySalary = calMonthlySalary;
-//}
-//
-//function calMonthlySalary(num_of_hours) {
-//	return this.salary_per_hour * num_of_hours;
-//}
-//
-//var player1 = new Employee("Wayne Rooney", "10/24/1985", 200);
-//var player2 = new Employee("Cristiano Ronaldo", "02/05/1985", 260);
-//var player3 = new Employee("Lionel Messi", "06/24/1987", 280);
-//
-//document.write("<p>Salary of the players this month:" + "</p>");
-//document.write("<p>" + player1.name + "&emsp;" + player1.dob + "&emsp;" + player1.calMonthlySalary(40) + "</p>");
-//document.write("<p>" + player2.name + "&emsp;" + player2.dob + "&emsp;" + player2.calMonthlySalary(35) + "</p>");
-//document.write("<p>" + player3.name + "&emsp;" + player3.dob + "&emsp;" + player3.calMonthlySalary(38) + "</p>");
+function Employee(name, dob, salary_per_hour) {
+	this.name = name;
+	this.dob = dob;
+	this.salary_per_hour = salary_per_hour;
+	this.calMonthlySalary = calMonthlySalary;
+}
+
+function calMonthlySalary(num_of_hours) {
+	return this.salary_per_hour * num_of_hours;
+}
+
+var player1 = new Employee("Wayne Rooney", "10/24/1985", 200);
+var player2 = new Employee("Cristiano Ronaldo", "02/05/1985", 260);
+var player3 = new Employee("Lionel Messi", "06/24/1987", 280);
+
+document.write("<p>Salary of the players this month:" + "</p>");
+document.write("<p>" + player1.name + "&emsp;" + player1.dob + "&emsp;" + player1.calMonthlySalary(40) + "</p>");
+document.write("<p>" + player2.name + "&emsp;" + player2.dob + "&emsp;" + player2.calMonthlySalary(35) + "</p>");
+document.write("<p>" + player3.name + "&emsp;" + player3.dob + "&emsp;" + player3.calMonthlySalary(38) + "</p>");
 
 //topic Function
-//var nums = [4, 4, 7, 23, 65, 78, 79, 95, 100, 110, 245, 678];
-//
-//function find(i, j, num) {
-//	if (i >= j) {
-//		if (nums[i] == num) {
-//			return true;
-//		}
-//	} else {
-//		var k = ((i + j) / 2) | 0;
-//		var part1 = find(i, k, num);
-//		return part1 ? part1 : find(k+1, j, num);
-//	}
-//	return false;
-//}
-//document.write("nums = [4, 4, 7, 23, 65, 78, 79, 95, 100, 110, 245, 678]");
-//document.write("<p>" + "7 is in the nums?" + "&emsp;" + find(0, nums.length -1 , 7) + "</p>");
-//document.write("<p>" + "101 is in the nums?" + "&emsp;" + find(0, nums.length -1, 101) + "</p>");
+var nums = [4, 4, 7, 23, 65, 78, 79, 95, 100, 110, 245, 678];
+
+function find(i, j, num) {
+	if (i >= j) {
+		if (nums[i] == num) {
+			return true;
+		}
+	} else {
+		var k = ((i + j) / 2) | 0;
+		var part1 = find(i, k, num);
+		return part1 ? part1 : find(k+1, j, num);
+	}
+	return false;
+}
+document.write("nums = [4, 4, 7, 23, 65, 78, 79, 95, 100, 110, 245, 678]");
+document.write("<p>" + "7 is in the nums?" + "&emsp;" + find(0, nums.length -1 , 7) + "</p>");
+document.write("<p>" + "101 is in the nums?" + "&emsp;" + find(0, nums.length -1, 101) + "</p>");
 
 //topic Event
 function showSchool() {
@@ -55,13 +55,13 @@ function showSchool() {
 }
 
 //topic Array
-//var stringArray = ["WayNe", "RoONey", "CriStiAnO", "RonalDo", "LiOnEl", "Messi"];
-//function lowerCase(str) {
-//	return str.toLowerCase();
-//}
-//
-//var lowerCaseStringArray = stringArray.map(lowerCase);
-//document.write("<p>" + lowerCaseStringArray + "</p>");
+var stringArray = ["WayNe", "RoONey", "CriStiAnO", "RonalDo", "LiOnEl", "Messi"];
+function lowerCase(str) {
+	return str.toLowerCase();
+}
+
+var lowerCaseStringArray = stringArray.map(lowerCase);
+document.write("<p>" + lowerCaseStringArray + "</p>");
 
 //topic Inheritance
 var Vehicle = function(name, velocity) {
@@ -94,8 +94,8 @@ setInheritance(Truck, Vehicle);
 var car = new Car("Car", 35);
 var truck = new Truck("Truck", 25);
 
-//car.run();
-//truck.run();
+car.run();
+truck.run();
 
 //topic Condition
 var numList = [6, -3, 14, 5, -6, 2, 3];
@@ -109,8 +109,8 @@ var compare = function(item, index) {
 	}
 }
 numList.forEach(compare);
-//document.write("<p>numList = [6, -3, 14, 5, -6, 2, 3]</p>");
-//document.write("<p>" + "The greatest number is: " + greatestNum + "&emsp;" + "The smalles number is: " + smallestNum + "</p>");
+document.write("<p>numList = [6, -3, 14, 5, -6, 2, 3]</p>");
+document.write("<p>" + "The greatest number is: " + greatestNum + "&emsp;" + "The smalles number is: " + smallestNum + "</p>");
 
 //topic Regular Expression
 var files = ["resume.txt", "financial.csv", "book.ltxt", "employee.txt", "habit.pdf", "abctxt"];
@@ -123,8 +123,8 @@ for (var i = 0; i < files.length; i++) {
 		txtFile.push(files[i]);
 	}
 }
-//document.write("<p>files = [\"resume.txt\", \"financial.csv\", \"book.ltxt\", \"employee.txt\", \"habit.pdf\", \"abctxt\"]</p>");
-//document.write("<p>List txt files: " + txtFile +  "</p>");
+document.write("<p>files = [\"resume.txt\", \"financial.csv\", \"book.ltxt\", \"employee.txt\", \"habit.pdf\", \"abctxt\"]</p>");
+document.write("<p>List txt files: " + txtFile +  "</p>");
 
 
 //topic Strict mode
@@ -137,7 +137,7 @@ var strictFunction = function() {
 	document.write("<p> x: " + x + "</p>");
 }
 
-//strictFunction();
+strictFunction();
 
 
 //topic Error
@@ -150,7 +150,7 @@ function testRuntimeError() {
 	}
 }
 
-//testRuntimeError();
+testRuntimeError();
 
 //topic Type conversion
 var formular1 = "200+400";
@@ -179,8 +179,8 @@ var cal = function(formular) {
 	return null;
 }
 
-//document.write("<p>calculate 200+400= " + cal(formular1) + "</p>");
-//document.write("<p>calculate 500-50= " + cal(formular2) + "</p>");
+document.write("<p>calculate 200+400= " + cal(formular1) + "</p>");
+document.write("<p>calculate 500-50= " + cal(formular2) + "</p>");
 
 //topic JSON
 var reqs = "{\"requests\":[" + 
@@ -193,7 +193,7 @@ try {
 	for (var i = 0; i < reqObj.requests.length; i++) {
 		var clientName = reqObj.requests[i].client;
 		var time = new Date(reqObj.requests[i].time).getTime() / 1000;
-		//document.write("<p>Client: " + clientName + " time received: " +  reqObj.requests[i].time + "-->" + (now - time > 60 ? "timeout" : "valid") + "</p>");
+		document.write("<p>Client: " + clientName + " time received: " +  reqObj.requests[i].time + "-->" + (now - time > 60 ? "timeout" : "valid") + "</p>");
 	}
 } catch (e) {
 	// TODO: handle exception
@@ -232,5 +232,5 @@ var getTemporaryCart = function() {
 	}
 }
 
-//storeTemporaryCart();
-//getTemporaryCart();
+storeTemporaryCart();
+getTemporaryCart();
